28d6144aef35fd7ae4881668506c812e7e311eb1
SmartGlaxx <smartegbuchulem@gmail.com>
2021-10-27 12:31:46 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+// import Header from './components/header'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import {LandingPage, Signup, Login, ComfirmEmail, ForgotPassword, ErrorPage, CreateInvestment, Investments,
+  CreateUser, UserProfile, Invest } from './pages';
+import DashboardHome from './pages/Dashboards/dashboardHome';
+import Overlay from './pages/UtilPages/overlay';
+// import {Sidebar, SidebarMobile} from './components/';
+// import {InvestorSidebar, InvestorSidebarMobile} from './components';
+//import AuthWrapper from './pages/UtilPages/authWrapper'
+import { UseContext } from './contexts.js/context';
+
 
 function App() {
+  const {authenticated} = UseContext()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <BrowserRouter>
+    <Overlay />
+      <Switch>
+        <Route path='/' exact>
+          <LandingPage />
+        </Route>
+        <Route path='/login' exact>
+          <Login />
+        </Route>
+        <Route path='/signup' exact>
+          <Signup />
+        </Route>
+        <Route path='/comfirmemail' exact>
+          <ComfirmEmail />
+        </Route>
+        <Route path='/forgotPassword' exact>
+          <ForgotPassword />
+        </Route>
+        <Route path='/dashboard' exact>
+          <DashboardHome />
+        </Route>
+        <Route path='/createinvestment/:userId/:username' exact>
+          <CreateInvestment />
+        </Route>
+        <Route path='/invest/:userId/:username' exact>
+          <Invest />
+        </Route>
+        <Route path='/investment/:userId/:username/investments' exact>
+          <Investments />
+        </Route>
+        <Route path='/createuser' exact>
+          <CreateUser />
+        </Route>
+        <Route path='/users/:userId/:username' exact>
+          <UserProfile />
+        </Route>
+        
 
+        <Route path='*' exact>
+          <ErrorPage />
+        </Route>
+    </Switch>
+    </BrowserRouter>
+  );
+
+}
 export default App;
